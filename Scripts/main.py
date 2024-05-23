645ecc65cec9c7a60f430b47a969f474f5cf0355
Ricardo <ricardofoliveira61@gmail.com>
2024-05-24 00:07:39 +0100

    [UPDATE] main.py

--- Scripts/main.py
+++ Scripts/main.py
@@ -1,3 +1,24 @@
-import rpy2
-import os
+import inputs
+import subprocess
+
+first_run = True
+while True:
+    if first_run:
+        project, workdir, fastq_files, sofware, seq_tech, region = inputs.get_user_inputs()        
+        first_run = False
+
+    if sofware == "DADA2":
+
+        if seq_tech == "ILLUMINA":
+            subprocess.run(["python", "./scripts/illumina_r.py" , project,workdir ,fastq_files, region])
+
+
+    break
+
+
+
+
+
+
+
 
