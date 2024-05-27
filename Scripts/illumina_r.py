639a8567cead1f78d4f82d312764270f1f1ce789
Ricardo <ricardofoliveira61@gmail.com>
2024-05-27 17:31:24 +0100

    [UPDATE] illumina_r.py

--- Scripts/illumina_r.py
+++ Scripts/illumina_r.py
@@ -1,37 +1,24 @@
-import importlib.util
-import pip
 import sys
 import os
 import subprocess
-
-
-#if importlib.util.find_spec("rpy2") is not None: 
-#    print("The package rpy2 is installed.")
-#   import rpy2
-#    import rpy2.robjects
-#    from rpy2.robjects import r
-#else: 
-#    pip_ = input("The package rpy2 is not installed. Do you want to install it?[Y/N]: ")
-
-#    if pip_.upper() == "Y" :
-#        pip.main(["install","rpy2"])
-#        print("Package rpy2 installed successfully")
-#        import rpy2
-#        import rpy2.robjects
-#        from rpy2.robjects import r
-    
-#    else:
-#        print("The package rpy2 is necessary to carry on amplicon analysis using DADA2")
-#        exit()
-
-res = subprocess.call("Rscript Scripts/DADA_illumina_V3V4_V4.R", shell=True)
-
+import platform
 
 
 project = sys.argv[1]; workdir = sys.argv[2]; fastq_files = sys.argv[3]; region = sys.argv[4]
 
-print(f"Received project: {project}")
-print(f"Received working directory: {workdir}")
-print(f"Received fastq files directory: {fastq_files}")
-print(f"Received sequenced region: {region}")
+if platform.system() == "Windows":
+    r_script_path = os.path.join("C:/", "Program Files", "R", os.listdir("C:/Program Files/R")[0], "bin", "Rscript")
+    subprocess.call([r_script_path, "Scripts/DADA_illumina_V3V4_V4.R", project, workdir, fastq_files, region], shell=False)
+
+elif platform.system() == "Linux":
+    subprocess.call(["Rscript", "Scripts/DADA_illumina_V3V4_V4.R",project, workdir, fastq_files, region], shell=True)
+
+else:
+    print(f"Platform {platform.system()} not supported")
+    exit()
+
+
+
+
+
 
