0ab0603ae0c515e28f2de3e934debdc26eed9acc
Ricardo <ricardofoliveira61@gmail.com>
2024-05-28 23:48:47 +0100

    [UPDATE] main.py

--- Scripts/main.py
+++ Scripts/main.py
@@ -1,20 +1,29 @@
 import inputs
 
 
-first_run = True
+workdir = inputs.work_dir()
 while True:
-    if first_run:
-        project, workdir, fastq_files, sofware, seq_tech, region = inputs.get_user_inputs(first_run); first_run = False
 
-    else: project, fastq_files, sofware, seq_tech, region = inputs.get_user_inputs(first_run)   
+    state = inputs.action()
+    inputs.script(state, workdir)
 
-    inputs.script_2_run(project, workdir, fastq_files, sofware, seq_tech, region) 
 
-    ans =inputs.what_next()
-    if ans == 1:
-        continue
-    elif ans == 3:
-        exit()
 
-    print("Classification not avaivle for now")
-    exit()
+
+
+
+    # if first_run:
+    #     project, workdir, fastq_files, sofware, seq_tech, region = inputs.get_user_inputs(first_run); first_run = False
+
+    # else: project, fastq_files, sofware, seq_tech, region = inputs.get_user_inputs(first_run)   
+
+    # inputs.script_2_run(project, workdir, fastq_files, sofware, seq_tech, region) 
+
+    # ans =inputs.what_next()
+    # if ans == 1:
+    #     continue
+    # elif ans == 3:
+    #     exit()
+
+    # print("Classification not avaivle for now")
+    # exit()
