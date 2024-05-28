2713f69b9cba436453975460fb4df598d709e396
Ricardo <ricardofoliveira61@gmail.com>
2024-05-28 10:54:22 +0100

    [UPDATE] illumina_r,py

--- Scripts/illumina_r.py
+++ Scripts/illumina_r.py
@@ -7,8 +7,14 @@ import platform
 project = sys.argv[1]; workdir = sys.argv[2]; fastq_files = sys.argv[3]; region = sys.argv[4]
 
 if platform.system() == "Windows":
-    r_script_path = os.path.join("C:/", "Program Files", "R", os.listdir("C:/Program Files/R")[0], "bin", "Rscript")
-    subprocess.call([r_script_path, "Scripts/DADA_illumina_V3V4_V4.R", project, workdir, fastq_files, region], shell=False)
+
+    if len( os.listdir("C:/Program Files/R")) == 0:
+        print("The program R is not installed in this computer, please install it first")
+        exit()
+
+    else:
+        r_script_path = os.path.join("C:/", "Program Files", "R", os.listdir("C:/Program Files/R")[0], "bin", "Rscript")
+        subprocess.call([r_script_path, "Scripts/DADA_illumina_V3V4_V4.R", project, workdir, fastq_files, region], shell=False)
 
 elif platform.system() == "Linux":
     subprocess.call(["Rscript", "Scripts/DADA_illumina_V3V4_V4.R",project, workdir, fastq_files, region], shell=True)
@@ -16,9 +22,5 @@ elif platform.system() == "Linux":
 else:
     print(f"Platform {platform.system()} not supported")
     exit()
-
-
-
-
 
 
