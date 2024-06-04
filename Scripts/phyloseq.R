3c110a0a319ded8a64b2f09b9368a91f046452d8
Ricardo <ricardofoliveira61@gmail.com>
2024-06-04 19:54:38 +0100

    [UPDATE] phyloseq.R

--- Scripts/phyloseq.R
+++ Scripts/phyloseq.R
@@ -32,6 +32,7 @@ if (length(missing_packages) > 0) {
 library(phyloseq)
 library(Biostrings)
 library(dplyr)
+library(ggplot2)
 
 
 # creating R varivales from python inputs
@@ -42,9 +43,11 @@ asv_counts = as.character(commandArgs(TRUE)[4])
 tax_class = as.character(commandArgs(TRUE)[5])
 meta_samples = as.character(commandArgs(TRUE)[6])
 setwd(workdir)
+set.seed(123)
 
 
-tax_class = "C:\\Users\\ricar\\Desktop\\Teste\\assignment_results\\v4\\v4_classification_results.xlsx"
+tax_class = "C:\\Users\\ricar\\Desktop\\Teste\\assignment_results\\teste_v4\\teste_RDP_classification_results.xlsx"
+tax_class = "C:\\Users\\ricar\\Desktop\\Teste\\assignment_results\\teste_v4\\teste_SILVA_classification_results.xlsx"
 asv_seqs = "C:\\Users\\ricar\\Desktop\\Teste\\V4\\Results\\V4_asv_single_refseq.fa"
 asv_count = "C:\\Users\\ricar\\Desktop\\Teste\\V4\\Results\\V4_amplicon_results.xlsx"
 meta_samples = "C:\\Users\\ricar\\Desktop\\Teste\\V4\\meta_mock1.tsv"
@@ -68,10 +71,12 @@ read_data = function(filepath) {
 }
 
 
+# tranformar os nomes das colunas em minusculas
+
 # preparing the taxonomic classification dataframe
 tax = as.data.frame(read_data(tax_class))
 if ("confidence" %in% colnames(tax)){
-  taxonomic = tax[, !(colnames(tax) == "confidence")]
+  tax = tax[, !(colnames(tax) == "confidence")]
 }
 tax = as.matrix(tax)
 
@@ -89,7 +94,6 @@ meta = as.data.frame(read_data(meta_samples))
 # creating phyloseq object
 phylo = phyloseq(counts, seqs, sample_data(meta), tax_table(tax))
 phylo
- 
 
 
 # extracting samples without taxonomic classification
@@ -108,8 +112,10 @@ phylo_no_phylum
 #  Biostrings::writeXStringSet("./dada_not_class.fna", append=FALSE,
 #                              compress=FALSE, compression_level=NA, format="fasta")
 
+
+# remover os nas até à espéice
 # removing the asvs withou classification
-phylo_w_NA = phylo %>% subset_taxa(domain != "NA" | !is.na(domain))
+phylo_w_NA = phylo %>% subset_taxa (domain != "NA" | !is.na(domain))
 phylo_w_NA
 
 
@@ -118,9 +124,39 @@ phylo_bac_all = phylo_w_NA %>% subset_taxa( domain == "Bacteria" | is.na(domain)
 phylo_bac_all
 
 
-phylo_bac = filter_taxa(phylo_bac_all, function(x) sum(x > 10) > (0.04*length(x)), TRUE)
+phylo_bac = filter_taxa(phylo_bac_all, function(x) {sum(x > 0) > 1}, TRUE)
 
-plot_richness(phylo_bac, measures=c("Observed", "Simpson", "Shannon"))
-
+# fazer com e sem normalização
+# sem normalização (apenas filtragem)
+#plot_richness(phylo_bac, measures=c("Observed", "Simpson", "Shannon"))
+plot_richness(phylo_bac , measures = c("Observed", "Chao1", "Shannon", "Simpson", "InvSimpson", "Fisher"))
 
 phyloseq::plot_bar(phylo_bac, fill = "phylum")
+
+
+# como mocks faz sentido até genero
+phylo_bac = phyloseq::tax_glom(phylo_bac, "genus", NArm = TRUE)
+
+
+# normalização das contagens
+# normalização da profundidade de sequenciação
+dbac_rare = rarefy_even_depth(phylo_bac, sample.size = min(sample_sums(phylo_bac)), replace = FALSE)
+
+# normalização frequências relativas das reads
+dbacr = transform_sample_counts(phylo_bac, function(x) x/sum(x))
+
+
+phyloseq::plot_bar(dbac_rare, fill = "phylum") 
+
+
+phyloseq::plot_bar(dbacr, fill = "phylum") 
+
+
+phyloseq::plot_bar(dbacr, fill = "family")
+
+
+phyloseq::plot_bar(dbacr, fill = "genus")
+
+
+plot_richness(dbac_rare, measures=c("Chao1", "Shannon"))
+
